54b5a48bbfa7640f8ec3a118d933b0159b8cae96
Bjorkan <93771679+Bjorkan@users.noreply.github.com>
2023-10-30 15:35:57 +0100

    Copying straight of wiki out of rage lmao

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -6,13 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
-
+     
+	// an instance of our new item
 	public static final Item CUSTOM_ITEM = new Item(new FabricItemSettings());
 
+	
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
